cbdb7776459209dc11151c05836612e49b993ae6
rohan.gore <rohan.gore@cashfree.com>
2023-10-01 13:26:30 +0530

    Flattern array

---
+++ js-dsa/flatternArray.js
@@ -0,0 +1,21 @@
+const array = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]];
+
+const n = 1;
+
+const flatternArray = (arr, n) => {
+  let result = [];
+
+  for (let i of arr) {
+    if (Array.isArray(i) && n !== 0) {
+      const res = flatternArray(i, n - 1);
+
+      result.push(...res);
+    } else {
+      result.push(i);
+    }
+  }
+
+  return result;
+};
+
+console.log(flatternArray(array, n));
