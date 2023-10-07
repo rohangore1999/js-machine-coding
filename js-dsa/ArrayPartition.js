3c2f8cd3400bd79f7d691d095b7b2a8603583280
rohan.gore <rohan.gore@cashfree.com>
2023-10-07 14:06:12 +0530

    Removal of Element

---
+++ js-dsa/ArrayPartition.js
@@ -0,0 +1,15 @@
+const nums = [6, 2, 6, 5, 1, 2];
+
+const arrayPairSum = (nums) => {
+  let sum = 0;
+
+  const sortedNums = nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < sortedNums.length; i += 2) {
+    sum += sortedNums[i];
+  }
+
+  return sum;
+};
+
+console.log(arrayPairSum(nums));
---
+++ js-dsa/removeElement.js
@@ -0,0 +1,40 @@
+const nums = [3, 3];
+const val = 3;
+
+const removeElement = function (nums, val) {
+  const occur = nums.filter((num) => num === val);
+
+  for (let i = 0; i < occur.length; i++) {
+    let indexOfVal;
+
+    indexOfVal = nums.indexOf(val);
+    console.log({ indexOfVal });
+
+    if (indexOfVal === -1) return nums;
+
+    nums.splice(indexOfVal, 1);
+    console.log({ nums });
+  }
+
+  return nums.length;
+};
+
+console.log(removeElement(nums, val));
+
+// Approach putting all nums which are !== val to left [two pointer approach]
+// var removeElement = function(nums, val) {
+//     let left = 0;
+//     let right = nums.length - 1;
+
+//     while (left <= right) {
+//         if (nums[left] === val) {
+//             nums[left] = nums[right];
+//             right--;
+//         }
+//         else {
+//             left++;
+//         }
+//     }
+
+//     return left;
+// };
