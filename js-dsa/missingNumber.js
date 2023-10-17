b0b82e667e3a15d822f1f7b74663c5cca69847a5
rohan.gore <rohan.gore@cashfree.com>
2023-10-17 11:02:44 +0530

    Missing Number using bit manipulation

---
+++ js-dsa/missingNumber.js
@@ -0,0 +1,23 @@
+const nums = [9, 6, 4, 2, 3, 5, 7, 0, 1];
+
+const missingNumber = function (nums) {
+  let sortedNums = nums.sort((a, b) => a - b);
+
+  for (let i = 0; i <= sortedNums.length; i++) {
+    if (i !== sortedNums[i]) return i;
+  }
+};
+
+console.log(missingNumber(nums));
+
+const missingNumber_optimal = function (nums) {
+  let res = 0;
+
+  for (let i = 0; i <= nums.length; i++) {
+    res = i ^ nums[i] ^ res;
+  }
+
+  return res;
+};
+
+console.log(missingNumber_optimal(nums));
--- js-dsa/summaryRanges.js
+++ js-dsa/summaryRanges.js
@@ -3,31 +3,30 @@
 const nums = [0, 1, 2, 4, 5, 7];
 
 const summaryRanges = function (nums) {
-  //   let i = 0;
-  //   let j = 0;
-  //   let result = [];
-  //   let res = [];
-  //   while (j < nums.length) {
-  //     res.push(nums[j]);
-  //     j++;
-  //     if (nums[j] - nums[i] === 1) {
-  //       res.push(nums[j]);
-  //       i++;
-  //       j++;
-  //     } else {
-  //       result.push(res);
-  //       res = [];
-  //       i = j;
-  //     }
-  //   }
-  //   console.log(res);
-  //   return result;
-};
+  let idx = 0;
+  output = [];
 
-const summaryRanges_opt = function (nums) {
-  for (let i = 0; i < nums.length; i++) {
-    
+  while (idx < nums.length) {
+    let beg, last;
+
+    beg = nums[idx];
+
+    while (idx + 1 < nums.length && nums[idx + 1] === nums[idx] + 1) {
+      idx++;
+    }
+
+    last = nums[idx];
+
+    if (beg === last) {
+      output.push(beg + "");
+    } else {
+      output.push(beg + "->" + last);
+    }
+
+    idx++;
   }
+
+  return output;
 };
 
 console.log(summaryRanges_opt(nums));
