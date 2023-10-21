f49251f7a78de841c4b4716e651498076ae9eea2
rohan.gore <rohan.gore@cashfree.com>
2023-10-21 12:31:32 +0530

    JS - dsa

---
+++ js-dsa/findAllNumbersDisappearedArray.js
@@ -0,0 +1,35 @@
+const nums = [4, 3, 2, 7, 8, 2, 3, 1];
+
+// const findDisappearedNumbers = function (nums) {
+//   const res = [];
+
+//   let sortedNums = nums.sort((a, b) => a - b);
+//   console.log({ sortedNums });
+
+//   for (let i = 1; i <= nums.length; i++) {
+//     if (!sortedNums.includes(i)) {
+//       res.push(i);
+//     }
+//   }
+
+//   console.log({ res });
+// };
+
+const findDisappearedNumbers = function (nums) {
+  const res = [];
+  for (let i = 0; i < nums.length; i++) {
+    let num = Math.abs(nums[i]);
+    let idx = num - 1;
+    nums[idx] = Math.abs(nums[idx]) * -1;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      res.push(i + 1);
+    }
+  }
+
+  return res;
+};
+
+console.log(findDisappearedNumbers(nums));
---
+++ js-dsa/thirdMaxNumber.js
@@ -0,0 +1,29 @@
+const nums = [1, 2, 2, 5, 3, 5];
+
+const thirdMax = function (nums) {
+  // Initialise with lowest negative number
+  let fMax = -Number.MAX_SAFE_INTEGER,
+    sMax = -Number.MAX_SAFE_INTEGER,
+    tMax = -Number.MAX_SAFE_INTEGER;
+
+  for (i of nums) {
+    if (i > fMax) {
+      tMax = sMax;
+      sMax = fMax;
+      fMax = i;
+    } else if (i > sMax && i !== fMax) {
+      tMax = sMax;
+      sMax = i;
+    } else if (i > tMax && i !== sMax && i !== fMax) {
+      tMax = i;
+    }
+  }
+
+  if (tMax === -Number.MAX_SAFE_INTEGER) {
+    return fMax;
+  }
+
+  return tMax;
+};
+
+console.log(thirdMax(nums));
