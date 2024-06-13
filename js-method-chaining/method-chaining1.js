b4c35c7606509f1f3ccabab2cbc12694f6796cfe
rohan.gore <rohan.gore@cashfree.com>
2024-06-13 10:20:52 +0530

    JS code

--- js-method-chaining/method-chaining1.js
+++ js-method-chaining/method-chaining1.js
@@ -30,42 +30,69 @@
 // --------------------------------------------------------------------------------
 
 // Using Function
-const calculator1 = function () {
-  sum = 0;
+// const calculator1 = function () {
+//   sum = 0;
 
-  return {
-    add: function (num) {
-      sum += num;
+//   return {
+//     add: function (num) {
+//       sum += num;
 
-      console.log(this);
+//       console.log(this);
 
-      return this;
-    },
-    subtract: function (num) {
-      sum -= num;
+//       return this;
+//     },
+//     subtract: function (num) {
+//       sum -= num;
 
-      console.log(this);
+//       console.log(this);
 
-      return this;
-    },
-    divide: function (num) {
-      sum /= num;
+//       return this;
+//     },
+//     divide: function (num) {
+//       sum /= num;
 
-      console.log(this);
+//       console.log(this);
 
-      return this;
-    },
-    multiply: function (num) {
-      sum *= num;
+//       return this;
+//     },
+//     multiply: function (num) {
+//       sum *= num;
 
-      console.log(this);
+//       console.log(this);
 
-      return this;
-    },
-    total: () => sum,
-  };
+//       return this;
+//     },
+//     total: () => sum,
+//   };
+// };
+
+// let myCal = calculator1();
+// myCal.add(10).subtract(2).divide(2).multiply(5);
+// console.log(myCal.total());
+
+// --------------------------------------------------------------------------------
+
+// Using Class
+
+class Calculator {
+  constructor() {
+    this.sum = 0;
+  }
+}
+
+Calculator.prototype.add = function (num) {
+  console.log("this add ", this);
+  this.sum += num;
+
+  return this;
 };
 
-let myCal = calculator1();
-myCal.add(10).subtract(2).divide(2).multiply(5);
-console.log(myCal.total());
+Calculator.prototype.subtract = function (num) {
+  console.log("this sub ", this);
+  this.sum -= num;
+
+  return this;
+};
+
+let cal = new Calculator();
+console.log(cal.add(20).subtract(10));
